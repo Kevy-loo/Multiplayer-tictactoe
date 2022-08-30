2ebd96e8b85bd5e04b92298e70190d10c9e80255
kevin-liu <liu.kevin1997@gmail.com>
2022-08-30 16:57:54 -0400

    game room

--- src/App.js
+++ src/App.js
@@ -1,50 +1,48 @@
-import React, {useState} from 'react';
-import './App.css';
-import TicGame from './tictactoe/TicGame';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import { StreamChat } from 'stream-chat';
-import Cookies from 'universal-cookie';
-import { Chat } from 'stream-chat-react';
-
-
-
-
+import React, { useState } from "react";
+import "./App.css";
+import TicGame from "./tictactoe/TicGame";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import { StreamChat } from "stream-chat";
+import Cookies from "universal-cookie";
+import { Chat } from "stream-chat-react";
+import JoinGame from "./components/JoinGame";
 
 function App() {
   const cookies = new Cookies();
-  const token = cookies.get('token')
-  const client = StreamChat.getInstance(process.env.REACT_APP_API_KEY)
-  const [isAuth, setIsAuth] = useState(false)
+  const token = cookies.get("token");
+  const client = StreamChat.getInstance(process.env.REACT_APP_API_KEY);
+  const [isAuth, setIsAuth] = useState(false);
 
   const logOut = () => {
-    cookies.remove('token');
-    cookies.remove('userId');
-    cookies.remove('firstName');
-    cookies.remove('lastName');
-    cookies.remove('hashedPassword');
-    cookies.remove('channelName');
-    cookies.remove('username');
+    cookies.remove("token");
+    cookies.remove("userId");
+    cookies.remove("firstName");
+    cookies.remove("lastName");
+    cookies.remove("hashedPassword");
+    cookies.remove("channelName");
+    cookies.remove("username");
     client.disconnectUser();
-    setIsAuth(false)
-
-  }
+    setIsAuth(false);
+  };
 
   if (token) {
-      client.connectUser({
-      id: cookies.get("userId"),
-      name: cookies.get('username'),
-      firstName: cookies.get('firstName'),
-      lastName: cookies.get('lastName'),
-      hashPassword: cookies.get('hashPassword')
-    }, token
-    ).then((user) => {
-      console.log(user)
-      setIsAuth(true)
-    })
-  } 
-
-
+    client
+      .connectUser(
+        {
+          id: cookies.get("userId"),
+          name: cookies.get("username"),
+          firstName: cookies.get("firstName"),
+          lastName: cookies.get("lastName"),
+          hashPassword: cookies.get("hashPassword"),
+        },
+        token
+      )
+      .then((user) => {
+        console.log(user);
+        setIsAuth(true);
+      });
+  }
 
   return (
     <div className="App">
@@ -54,21 +52,19 @@ function App() {
       </Routes> */}
       {isAuth ? (
         <Chat client={client}>
-          <TicGame/>
+          <JoinGame />
           <button onClick={logOut}>Logout</button>
         </Chat>
-        ) : (
+      ) : (
         <>
-      <SignUp setIsAuth={setIsAuth}/>
-      <Login setIsAuth={setIsAuth}/>
-      </>
+          <SignUp setIsAuth={setIsAuth} />
+          <Login setIsAuth={setIsAuth} />
+        </>
       )}
-      
-      
+
       {/* <Routes>
         <Route path='/tictactoe' element={<TicGame/>}/>
       </Routes> */}
-      
     </div>
   );
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,59 +1,58 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import Cookies from 'universal-cookie';
+import React, { useState } from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
 
-
-
-function SignUp({setIsAuth}) {
+function SignUp({ setIsAuth }) {
   const cookies = new Cookies();
   const [user, setUser] = useState(null);
-  
 
   const signUp = () => {
-    axios.post('http://localhost:8000/signup', user).then((res) => {
-      const {token, userId, firstName, lastName, username, hashedPassword} = res.data;
-      console.log(token)
-      cookies.set('token', token)
-      cookies.set('userId', userId)
-      cookies.set('firstName', firstName)
-      cookies.set('lastName', lastName)
-      cookies.set('username', username)
-      cookies.set('hashedPassword', hashedPassword)
-      setIsAuth(true)
-
-    })
+    axios.post("http://localhost:8000/signup", user).then((res) => {
+      const { token, userId, firstName, lastName, username, hashedPassword } =
+        res.data;
+      cookies.set("token", token);
+      cookies.set("userId", userId);
+      cookies.set("firstName", firstName);
+      cookies.set("lastName", lastName);
+      cookies.set("username", username);
+      cookies.set("hashedPassword", hashedPassword);
+      setIsAuth(true);
+    });
   };
   return (
-    <div className='signup'>
+    <div className="signup">
       <label>SignUp</label>
-      <input 
-        placeholder='First Name'
-        type='text' onChange={(event) => {
-          setUser({...user, firstName: event.target.value})
-        }}
-      />
-      <input 
-        placeholder='Last Name'
-        type='text' onChange={(event) => {
-          setUser({...user, lastName: event.target.value})
-        }}
-      />
-      <input 
-        placeholder='Username'
-        type='text' onChange={(event) => {
-          setUser({...user, username: event.target.value})
-        }}
-      />
-      <input 
-        placeholder='Password' 
-        type='password'
+      <input
+        placeholder="First Name"
+        type="text"
         onChange={(event) => {
-          setUser({...user, password: event.target.value})
+          setUser({ ...user, firstName: event.target.value });
         }}
       />
-        <button onClick={signUp}>Submit</button>
+      <input
+        placeholder="Last Name"
+        type="text"
+        onChange={(event) => {
+          setUser({ ...user, lastName: event.target.value });
+        }}
+      />
+      <input
+        placeholder="Username"
+        type="text"
+        onChange={(event) => {
+          setUser({ ...user, username: event.target.value });
+        }}
+      />
+      <input
+        placeholder="Password"
+        type="password"
+        onChange={(event) => {
+          setUser({ ...user, password: event.target.value });
+        }}
+      />
+      <button onClick={signUp}>Submit</button>
     </div>
-  )
+  );
 }
 
 export default SignUp;
